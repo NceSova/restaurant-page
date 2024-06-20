9bb1b756f844fef85272dbab8de9d1ab718da732
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-20 18:10:30 +0300

    fixed about layout and fonts spacing

--- src/index.js
+++ src/index.js
@@ -4,3 +4,23 @@ import getAboutDiv from "./about.js";
 const mainContainer = document.querySelector("#content");
 mainContainer.innerHTML = "";
 mainContainer.appendChild(getAboutDiv());
+
+const getPage = (id) => {
+  switch (id) {
+    case "about":
+      return getAboutDiv();
+      break;
+    case "menu":
+      return;
+      break;
+    case "contacts":
+      return;
+      break;
+    default:
+      return;
+      break;
+  }
+};
+
+const navMenu = document.querySelector(".links");
+navMenu.addEventListener("click", (e) => {});
